3984ff53c26a4e9e4d19965ae92253d8e0c0cca3
yanglin <linyang@tungee.com>
2021-07-26 15:42:10 +0800

    s

---
+++ helloworld/src/com/tungee/helloworld.java
@@ -0,0 +1,12 @@
+package com.tungee;
+
+/*
+  @Created by EDY
+  @Date 2021/7/26 15:04
+  @Description TODO
+ */
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("hellowrold");
+    }
+}
